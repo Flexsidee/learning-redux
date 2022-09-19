4d76e9958244c284e2acb62d3a3cf66b0b0e483a
flexsidee <flexsidee@gmail.com>
2022-09-19 14:55:06 +0100

    added store and icecreamsSlice

---
+++ rtk-demo/features/icecream/icecreamSlice.js
@@ -0,0 +1,21 @@
+const createSlice = require("@reduxjs/toolkit").createSlice;
+
+const initialState = {
+	numOfIceCreams: 10,
+};
+
+const icecreamSlice = createSlice({
+	name: "icecream",
+	initialState: initialState,
+	reducer: {
+		ordered: (state) => {
+			state.numOfIceCreams--;
+		},
+		restocked: (state, action) => {
+			state.numOfIceCreams += action.payload;
+		},
+	},
+});
+
+module.exports = icecreamSlice.reducer;
+module.exports.icecreamActions = icecreamSlice.actions;
--- rtk-demo/index.js
+++ rtk-demo/index.js
@@ -1,5 +1,7 @@
 const store = require("./app/store");
 const cakeActions = require("./features/cake/cakeSlice").cakeActions;
+const icecreamActions =
+	require("./features/icecream/icecreamSlice").icecreamActions;
 
 console.log("Inital state: ", store.getState());
 const unsubscribe = store.subscribe(() => {
@@ -11,5 +13,9 @@ store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.restocked(5));
+
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.restocked(3));
 
 unsubscribe();
